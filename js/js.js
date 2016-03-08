d44fb683062b2301a3a7637eddc680bab1a9e77b
wjw8860 <wjw8860@163.com>
2016-03-08 11:28:48 +0800

    12

--- js/js.js
+++ js/js.js
@@ -2,7 +2,15 @@
 /**
  * Created by wangjunwu on 2016/3/3.
  */
-function drag()
+function drag(id)
 {
+    var obj=document.getElementById(id);
+    var disX=0;
+    var disY=0;
 
+    obj.onmousedown=function(ev)
+    {
+        var ev=ev|| event;
+    };
 }
+//测试1
